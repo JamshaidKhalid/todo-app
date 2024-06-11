b1c31fd74a97ebf6b33c3ea9d393e7821884a946
Jamshaid Khalid <jamshaidkhalid32@gmail.com>
2024-06-11 22:25:42 +0500

    inital setup and database connection

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const dotenv = require('dotenv');
+const supabase = require('./config/supabaseClient');
+dotenv.config();
+
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.get('/', async (req, res) => {
+    try {
+      const { data, error } = await supabase.from('todos').select('*');
+      if (error) throw error;
+      res.json(data);
+    } catch (err) {
+      console.error('Error fetching data:', err);
+      res.status(500).send('Database connection failed');
+    }
+  });
+
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+    }
+);
