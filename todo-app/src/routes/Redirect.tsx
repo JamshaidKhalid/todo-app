e8ddc2af7ba1400b9caf5a4f955b3bcd08f261a8
jamshaid-khalid <jamshaid.cortechsols@gmail.com>
2024-07-18 16:44:29 +0500

    routing

--- todo-app/src/routes/Redirect.tsx
+++ todo-app/src/routes/Redirect.tsx
@@ -1,29 +1,28 @@
 import { useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 import AuthPage from '../pages/AuthPage';
 import TodosPage from '../pages/TodoPage';
 
-
 const Redirect: React.FC = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const isLoggedIn = localStorage.getItem('authToken');
-
 
   useEffect(() => {
     if (isLoggedIn) {
       navigate('/dashboard');
-    } else {
+    } else if (location.pathname !== '/signup') {
       navigate('/');
     }
-  }, [navigate]);
+  }, [navigate, isLoggedIn, location.pathname]);
 
   return (
     <>
-    <Routes>
-    <Route path="/dashboard" element={<TodosPage />} />
-          <Route path="/" element={<AuthPage formType='signin' />} />
-          <Route path="/signup" element={<AuthPage formType='signup' />} />
-    </Routes>
+      <Routes>
+        <Route path="/dashboard" element={<TodosPage />} />
+        <Route path="/" element={<AuthPage formType='signin' />} />
+        <Route path="/signup" element={<AuthPage formType='signup' />} />
+      </Routes>
     </>
   );
 };
